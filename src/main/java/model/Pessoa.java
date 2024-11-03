3d39da1353e22e830dd463ab52c2637edfbd6652
pauulohfs <pauulohfs@gmail.com>
2024-11-03 01:29:12 -0300

    Anotations Pessoa

--- src/main/java/model/Pessoa.java
+++ src/main/java/model/Pessoa.java
@@ -4,6 +4,7 @@
  */
 package model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public abstract class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idPessoa;
+    @Column
     private String nome;
     private String cpf;
     private Date dtNasc;
